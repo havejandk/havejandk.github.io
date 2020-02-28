4dfa74f17ddf80695dee149c55ad0559b7eebaa3
hej <helge.jensen@slog.dk>
2020-02-28 08:14:12 +0100

    Moved files out of docs folder

---
+++ image_library.js
@@ -0,0 +1,15 @@
+  var images = {
+    "Beplantning": [ "DIGITAL_4_014.JPG", "DIGITAL_3_032.JPG", 
+       "DIGITAL_5_020.JPG", "DIGITAL_5_021.JPG", 
+       "Diverse fra mormors kamara 005.JPG", "P1010185.JPG", "P1010193.JPG" ],
+    "Ild og Vand": ["DIGITAL_2_040.JPG", "DIGITAL_1_003.JPG",
+       "DIGITAL_3_004.JPG", "DIGITAL_4_008.JPG" ],
+    "Indkørsler": [ "DIGITAL_5_019.jpg", "Diverse 28-3 047.jpg" ],
+    "Terrænmure": [ "DIGITAL_4_039.JPG", "Diverse fra mormors kamara 021.JPG", 
+      "P1190176.JPG" ],
+    "Trapper": [ "24.11.07 11.03.JPG", "DIGITAL_4_002.JPG", 
+      "Diverse 28-3 054.JPG"],
+    "Træarbejde": ["DIGITAL_3_018.JPG", "Diverse 28-3 059.JPG"]
+  };
+  var thumbprefix = "thumb/";
+  var albumprefix = "album/";
